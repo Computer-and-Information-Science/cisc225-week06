16cfdd72fe79586b244bbd90f4f71525f058cc98
Norm Lippincott <nl@acm.org>
2023-09-26 04:48:36 -0400

    Added two demonstration programs

---
+++ print-binary.asm
@@ -0,0 +1,32 @@
+; Print binary value of AL Register to console
+; Northampton Community College
+; CISC 225, Computer Organization
+
+global _start		; exposes program entry point to the linker
+
+section .text		; start of code segment
+
+_start:
+  ; Sample value in AL
+  mov AL,0xa6
+
+
+
+	; Write string to the console
+	mov		rax, 1					; system call for write
+	mov		rdi, 1					; file handle, stdout
+	mov		rsi, al_str			; address of string to output
+	mov		rdx, al_str_len		; number of bytes to output
+	syscall							; invoke operating system call
+
+	; End the program
+	mov		rax, 0x3c				; system call for exit
+	xor		rdi, rdi				; exit code 0
+	syscall							; invoke operating system call
+
+section .data		; start of initialized data segment
+
+	al_str db "xxxxxxxx",0xa		; string with newline
+	al_str_len equ $-al_str	    ; length of hello_str
+
+section .bss		; start of uninitialized data segment
---
+++ print-hexadecimal.asm
@@ -0,0 +1,34 @@
+; Print hexadecimal value of a quadword
+; Northampton Community College
+; CISC 225, Computer Organization
+
+global _start		; exposes program entry point to the linker
+
+section .text		; start of code segment
+
+_start:
+
+
+
+
+
+	; Write ASCII of num to the console
+	mov		rax, 1					      ; system call for write
+	mov		rdi, 1			      		; file handle, stdout
+	mov		rsi, number_str  			; address of string to output
+	mov		rdx, number_str_len		; number of bytes to output
+	syscall					        		; invoke operating system call
+
+	; End the program
+	mov		rax, 0x3c				; system call for exit
+	xor		rdi, rdi				; exit code 0
+	syscall							; invoke operating system call
+
+section .data		; start of initialized data segment
+
+  number dq 2764921
+
+section .bss		; start of uninitialized data segment
+
+  number_str_len equ 21
+  number_str resb number_str_len ; output string, 20 digits plus newline
